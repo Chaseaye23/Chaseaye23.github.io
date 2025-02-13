0f9712a0e23dfec587cd4ecb4269fa158115e8fb
[Chase DeMey] <[manutdchase@gmail.com]>
2025-02-12 22:33:57 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-let clicks = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    let counter = 0;
+    const button = document.getElementById("button");
+    const display = document.getElementById("counter");
 
-function click(){
-    clicks++;
-}
+    button.textContent = "Click me";
+    display.textContent = `Counter: ${counter}`;
 
-function render(){
-    document.getElementById('counter').innerHTML = clicks;
-}
+    button.addEventListener("click", function () {
+        counter++;
+        display.textContent = `Counter: ${counter}`;
+    });
+
+    document.body.appendChild(display);
+    document.body.appendChild(button);
+});
