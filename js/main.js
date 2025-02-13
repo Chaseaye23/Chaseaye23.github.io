d0f1ee557aca3ab30fda8ea708c89048af8eb503
[Chase DeMey] <[manutdchase@gmail.com]>
2025-02-12 23:00:52 -0500

    update

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
     
     const clickButton = document.getElementById("clickButton");
     const counterLabel = document.getElementById("counter");
-    const upgradePlusOne =  document.getElementById("upgradePlusOne")
-    const clickValueLabel =  document.getElementById("clickValue")
+    const upgradePlusOne =  document.getElementById("upgradePlusOne") //button to add 1 value per click
+    const clickValueLabel =  document.getElementById("clickValue") //displays click value
 
     clickButton.textContent = "Click me";
     upgradePlusOne.textContent = "Increase Click Value";
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
         counterLabel.textContent = `Counter: ${counter}`;
     });
 
-    clickValueLabel.addEventListener("click", function () {
+    upgradePlusOne.addEventListener("click", function () {
         clickValue++;
         clickValueLabel.textContent = `Click Value: ${clickValue}`;
     });
