cab0e2363bc2e64a4768cb38fd08e8caf5f8cf79
[Chase DeMey] <[manutdchase@gmail.com]>
2025-02-12 23:19:01 -0500

    red

--- js/main.js
+++ js/main.js
@@ -1,32 +1,21 @@
 let counter = 0;
-let clickValue = 1;
 
 document.addEventListener("DOMContentLoaded", function () {
     
     const clickButton = document.getElementById("clickButton");
     const counterLabel = document.getElementById("counter");
-    const upgradePlusOne =  document.getElementById("upgradePlusOne") 
-    const clickValueLabel =  document.getElementById("clickValue")
 
     clickButton.textContent = "Click me";
-    upgradePlusOne.textContent = "Increase Click Value";
     counterLabel.textContent = `Counter: ${counter}`;
-    clickValueLabel.textContent = `Click Value: ${clickValue}`;
 
     clickButton.addEventListener("click", function () {
-        counter = counter + clickValue;
+        counter++;
         counterLabel.textContent = `Counter: ${counter}`;
     });
-
-    upgradePlusOne.addEventListener("click", function () {
-        clickValue++;
-        clickValueLabel.textContent = `Click Value: ${clickValue}`;
-    });
+    
 
     document.body.appendChild(counterLabel);
     document.body.appendChild(clickButton);
-    document.body.appendChild(upgradePlusOne);
-    document.body.appendChild(clickValueLabel);
 });
 
 function test(){
